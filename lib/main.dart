a1787a366dcd330c73e14667b928ea77f668b091
Simmi Kumari <simmikumari942@gmail.com>
2026-02-16 17:54:31 -0500

    Updated main.dart with skeleton code

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stateful Lab',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CounterWidget(),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0; // This is our STATE
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Interactive Counter')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              color: Colors.blue.shade100,
+              padding: EdgeInsets.all(20),
+              child: Text(
+                '$_counter',
+                style: TextStyle(fontSize: 50.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Slider(
+            min: 0, max: 100,
+            value: _counter.toDouble(),
+            onChanged: (double value) {
+              // 👇 This triggers the UI rebuild
+              setState(() {
+                _counter = value.toInt();
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
